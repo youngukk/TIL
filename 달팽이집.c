896b3a8a86befd854eb9d2f0d4e7b139a9306b36
Younguk Kim <dev.younguk@gmail.com>
2021-07-25 21:13:33 +0900

    달팽이

---
+++ 달팽이집.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n = 0, s = 1, k = 5, i = 0, j = -1;
+	int a[5][5] = { {0,} ,{0,} };
+	while (1)
+	{
+		for (int p = 0; p < k; p++)
+		{
+			n += 1;
+			j += s;
+			a[i][j] = n;
+		}
+		k -= 1;
+
+		if (k != 0)
+		{
+			for (int p = 0; p < k; p++)
+			{
+				n += 1;
+				i += s;
+				a[i][j] = n;
+			}
+			s *= -1;
+		}
+
+		else break;
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+
+	}
+	return 0;
+}
