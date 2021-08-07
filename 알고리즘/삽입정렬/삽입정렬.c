3e262de6d9a216f9f6fb64a7f580b24505686c5b
Younguk Kim <dev.younguk@gmail.com>
2021-08-07 23:26:55 +0900

    insertion_sort

---
+++ 알고리즘/삽입정렬/삽입정렬.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int main()
+{
+
+	int i, j, k, key;
+	int a[5] = {30, 70, 10, 90, 40};
+	j = -1;
+
+
+
+	for (i = 1; i < 5; i++)
+	{
+		key = a[i];
+		for (k = i -1; k >= 0; k--)
+		{
+			if (a[k] > key)
+				a[k + 1] = a[k];
+			else break;
+		}
+		a[k + 1] = key;
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
